988a745f46ba23dc1d66c033f2f10c48bcbddcce
Tanya <2248916k@student.gla.ac.uk>
2018-03-09 03:15:46 +0000

    example events added in populations script

--- population_script.py
+++ population_script.py
@@ -6,11 +6,36 @@ import django
 django.setup()
 from bark.models import User,  DogOwner, Event, FoodMenu, Rating, ChooseAnEvent
 
-
 def populate():
+    dog_events = [
+        {"title": "Disco Party",
+         "theme": "80s",
+         "capacity": "12",
+         "date":"2018-03-29",
+         "start":"16:30:00",
+         "end":"20:30:00"
+
+    },
+         {"title": "Hawaii party",
+         "theme": "Coconut",
+         "capacity": "18",
+         "date": "2018-04-29",
+         "start": "17:30:00",
+         "end": "20:30:00"
+    }
+    ]
 
 
-    def add_event(title,theme,capacity,date,start,end):
+    for event in dog_events:
+
+        c = add_event( event["title"], event["theme"],event["capacity"],event["date"],event["start"],event["end"])
+
+
+
+
+
+
+def add_event(title,theme,capacity,date,start,end):
         c = Event.objects.get_or_create(title=title,theme=theme,capacity=capacity,date=date,start=start,end=end)[0]
         c.theme=theme
         c.capacity = capacity
@@ -21,6 +46,6 @@ def populate():
         return c
 
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         print("Starting Bark population scripts...")
         populate()
