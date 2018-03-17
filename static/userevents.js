19b1ef30fdde32543c97d5bdbb23996c015c9022
Tanya <2248916k@student.gla.ac.uk>
2018-03-17 00:48:15 +0000

    made show my events prettier

--- static/userevents.js
+++ static/userevents.js
@@ -2,6 +2,14 @@ function getevents(){
     var eventchar = document.getElementById("id_events").value;
     temp = String(eventchar).split(";");
     for (i = 0; i < temp.length; i++) {
-        document.write(temp[i] + "<br >");
+
+        var para = document.createElement("p");
+        var node = document.createTextNode(temp[i]);
+        para.appendChild(node);
+
+        var element = document.getElementById("div1");
+        var child = document.getElementById("p1");
+        element.insertBefore(para,child);
+        document.getElementById("b").disabled = true;
     }
 }
