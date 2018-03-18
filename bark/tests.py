8bbc042c5fe35975d52f0c4f9eebcc1e8d292a13
Martin Dimitrov <2247164D@student.gla.ac.uk>
2018-03-18 20:16:48 +0000

    more unit tests added

--- bark/tests.py
+++ bark/tests.py
@@ -5,7 +5,7 @@ from django.test.utils import setup_test_environment
 import os
 from django.contrib.auth.models import User
 from bark.forms import UserForm,OwnerForm,OrganizerForm
-from bark.models import UserProfile, FoodMenu
+from bark.models import UserProfile, FoodMenu, Event
 from django.conf import settings
 def create_user():
     # Create a user
@@ -46,8 +46,20 @@ class TemplateTests(TestCase):
 
 
     def test_index_displays_no_events_message(self):
-        # Access index with empty database
         response = self.client.get(reverse('index'))
-
-        # Check if no categories message is displayed
         self.assertIn("There are no events present.".lower(), response.content.decode('ascii').lower())
+
+    def test_contact_page_using_template(self):
+        response = self.client.get(reverse('contact'))
+        self.assertTemplateUsed(response,'base.html')
+
+class ModelTests(TestCase):
+    def test_create_a_new_event(self):
+        event = Event(title="Joanne",capacity=30,date="2018-03-29",start="17:30:00",end="20:00:00",organizerusername="Lady Gaga")
+        event.save()
+
+        # Check event is in database
+        events_in_database = Event.objects.all()
+        self.assertEquals(len(events_in_database), 1)
+        only_event_in_database = events_in_database[0]
+        self.assertEquals(only_event_in_database, event)
